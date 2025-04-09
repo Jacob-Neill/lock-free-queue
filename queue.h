64a6c0e0eecc434924e73e0c0e298ddc0b12ee0d
Jacob-Neill <neillrjacob@gmail.com>
2025-04-09 13:27:19 -0500

    Change memory reclamation scheme

--- queue.h
+++ queue.h
@@ -4,7 +4,7 @@
 template<class T>
 struct Node {
     std::atomic<T*> data;
-    std::atomic<Node*> next;
+    std::atomic<Node<T>*> next;
 
     Node():data{nullptr}, next{nullptr} {}
     ~Node() {delete data.load()};//next is a pointer whose value will deallocate and destruct itself
@@ -12,12 +12,19 @@ struct Node {
 
 template<class T>
 class Queue {//Micheal Scott queue
-    std::atomic<Node*> head;
-    std::atomic<Node*> tail;
-    std::atomic<Node*> toDelete;//This is a lock-free bag of nodes saving nodes for deletion
+    std::atomic<Node<T>*> head;
+    std::atomic<Node<T>*> tail;
+    std::atomic<Node<T>*> toDelete;//This is a lock-free bag of nodes saving nodes for deletion
+    std::atomic<int> threadsInPop;
     void restoreQueue();
+    void tryReclaim(Node<T>* oldHead);
 public:
-    Queue(): head{new Node}, tail{head.load()}, toDelete{nullptr} {}
+    Queue(): {
+        head.store(new Node<T>());
+		tail = head.load();
+        toDelete.store(nullptr);
+		threadsInPop.store(0);
+    }
     ~Queue();
     void push(T data);
     std::optional<T> pop();
@@ -31,9 +38,9 @@ Queue<T>::~Queue() {
     }
     delete head;//delete the dummy node
 
-    Node* current = toDelete;
+    Node<T>* current = toDelete;
     while (current != nullptr) { //perform the actual deletion
-        Node* next = current->next;
+        Node<T>* next = current->next;
         delete current;
         current = next;
     }
@@ -41,9 +48,9 @@ Queue<T>::~Queue() {
 
 template<class T>
 void Queue<T>::restoreQueue() {
-    Node* newNode = new Node();
-	Node* oldTail = tail;//all operations must be taken on a local copy of tail
-	Node* null = nullptr;
+    Node<T>* newNode = new Node();
+	Node<T>* oldTail = tail;//all operations must be taken on a local copy of tail
+	Node<T>* null = nullptr;
 
     while (oldTail->data != nullptr) {
         if (oldTail->next.compare_exchange_strong(null, newNode)) { //second linearization point
@@ -64,7 +71,7 @@ template<class T>
 void Queue<T>::push(T data) {
     T* newData = new T (std::move(data));
     bool inserted = false;
-    Node* null = nullptr;
+    T* null = nullptr;
     while (!inserted) {
         if (tail->data.compare_exchange_strong(null, newData)) { //first linearization point
             inserted = true;
@@ -78,16 +85,49 @@ void Queue<T>::push(T data) {
 }
 
 template<class T>
+void Queue<T>::tryReclaim(Node<T>* oldHead) {
+    if (threadsInPop == 1) {//only thread in pop
+		Node<T>* oldToDelete = toDelete.exchange(nullptr);//claim the toDelete bag
+		if (!--threadsInPop) {//sole thread with access to the nodes in toDelete
+			//delete everything in the bag
+            while (oldToDelete != nullptr) {
+				Node<T>* current = oldToDelete;
+				oldToDelete = oldToDelete->next;
+				delete current;
+            }
+		}
+        else {
+            //replace the nodes
+            while (current != nullptr) {
+			Node<T>* current = oldToDelete;
+			oldToDelete = current->next;
+			Node<T>* curToDelete = toDelete;
+			current->next = curToDelete;
+            while (!toDelete.compare_exchange_weak(curToDelete, current));
+            }
+        }
+        delete oldHead;//this is safe to delete because it is not in either list
+    }
+    else {
+        //put oldHead in the toDelete bag
+        oldHead->next = toDelete;
+        while (!toDelete.compare_exchange_weak(oldHead->next, oldHead));
+        --threadsInPop;
+    }
+}
+
+template<class T>
 std::optional<T> Queue<T>::pop() {
     if(empty()) return std::nullopt;//Cannot pop the dummy node
+	threadsInPop++;
 
-    Node* oldHead = head;
+    Node<T>* oldHead = head;
     do {
         if (oldHead->next == nullptr) return std::nullopt;//Don't pop the dummy node
     } while (!head.compare_exchange_weak(oldHead, oldHead->next));
 
-    oldHead->next = toDelete;
-    while(!toDelete.compare_exchange_weak(oldHead->next, oldHead));
+    T data = *(oldHead->data);
+    tryReclaim(oldHead);
 
-    return *(oldHead->data);
+    return data;
 }
